00f222e458a7b29a1613c3c516e76f24a36ed134
Dovud Qurbonov <dovudqurbonov07@gmail.com>
2021-03-27 00:18:42 +0500

    add example_test

--- pkg/bank/payment/example_test.go
+++ pkg/bank/payment/example_test.go
@@ -6,3 +6,26 @@ import (
 )
 
 
+func ExamplePaymentSources(){
+	cards := []types.Card{
+		{
+			Active: true,
+			Balance: 10_000_00,
+			PAN: "5058 xxxx xxxx 9999",
+		},{
+			Active: false,
+			Balance: 10_00,
+			PAN: "5058 xxxx xxxx 8888",
+		},{
+			Active: true,
+			Balance: -10_00,
+			PAN: "5058 xxxx xxxx 8989",
+		},
+	}
+
+	payments := PaymentSources(cards)
+	for _, payment := range payments {
+		fmt.Println(payment.Number)
+	}
+
+}
