231521c1d64658ee38e12de8be54af743049f0cd
Dovud Qurbonov <dovudqurbonov07@gmail.com>
2021-03-27 00:23:58 +0500

    cear main.go

--- cmd/bank/main.go
+++ cmd/bank/main.go
@@ -1,45 +1,7 @@
 package main
 
 import (
-	"fmt"
-	"bank/pkg/bank/types"
-	_ "bank/pkg/bank/payment"
-
 )
 
-func main () {
-	payments := []types.Payment {
-		{
-			ID: 1,
-			Amount: 10,
-		},{
-			ID: 2,
-			Amount: 11,
-		},{
-			ID: 3,
-			Amount: 50,
-		},{
-			ID: 4,
-			Amount: 99,
-		},{
-			ID: 5,
-			Amount: 55,
-		},
-	}
-	max := Max(payments)
-	fmt.Println(max.ID)
-	//Output: 3
-
-}
-
-// Max ...
-func Max( payments []types.Payment) (types.Payment) {
-	
-	max := payments[0]
-	for _, payment := range payments {
-		if max.Amount < payment.Amount {
-			max = payment
-		}
-	}
-	return max
+func main () {	
 }
